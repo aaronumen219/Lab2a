9331920ab932df39d5fdba3b3d907cf350bbfc44
aaronumen219 <aaronumen219@gmail.com>
2020-09-10 15:28:36 +0000

    Wrote the code

--- main.cs
+++ main.cs
@@ -2,6 +2,15 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Enter the name of item");
+    string name = Console.ReadLine();
+    
+  
+    Console.WriteLine("Enter the quantity");
+    int quantity = Convert.ToInt32(Console.ReadLine());
+   
+    Console.WriteLine("Enter the price");
+    double cost = Convert.ToDouble((Console.ReadLine()));
+    Console.WriteLine("Total price of " + name + " = " + quantity * cost);
   }
 }
